8f4f678ef6951ef3ff4b64d608d3f8e1962bf776
Austin Slater <austinslater58@gmail.com>
2025-05-01 00:40:34 -0300

    added basic logic for the todo application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { throwIfEmpty } from 'rxjs';
+
+interface Task {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 
 @Component({
   selector: 'app-root',
@@ -8,5 +16,28 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'todo-app';
+  tasks: Task[] = []
+  newTaskTitle: string = "";
+
+  addTask(){
+    // check if there is a new task
+    if (this.newTaskTitle.trim()){
+      this.tasks.push({
+        id: this.tasks.length,
+        title: this.newTaskTitle,
+        completed: false
+      });
+
+      // Clear the input
+      this.newTaskTitle = '';
+    }
+  }
+
+  toggleTaskCompletion(task: Task){
+    task.completed = !task.completed;
+  }
+
+  deleteTask(id:number){
+    this.tasks = this.tasks.filter(task => task.id !== id)
+  }
 }
